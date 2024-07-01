73dda2e6d1a4524eec9e10ec939ce613b29b9024
ABigJuicyBurger <70484524+ABigJuicyBurger@users.noreply.github.com>
2024-07-01 10:13:19 -0600

    added number and operator variable

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Define number operator and another number
+let firstNumber = a;
+let secondNumber = b;
+let Operator;
+
+// Functions for all basic math operators on calculators
 function add(a, b) {
   return a + b;
 }
